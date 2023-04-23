73b7f6e58ef8da76a9b6795dd34b612c9beb7678
Akash prajapati <114556602+akash-buddy@users.noreply.github.com>
2023-04-23 21:11:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ y_test=y_test*scale_factor
 st.subheader("Predictions vs Original")
 fig2 = plt.figure(figsize=(12,6))
 plt.plot(y_test,'b',label='Original Price')
-plt.plot(y_predicted,'g',label='Predicted Price')
+plt.plot(y_predicted,'r',label='Predicted Price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 st.pyplot(fig2)
